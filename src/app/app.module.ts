6f063f3167efb92f5c45c12a63439b4e13be773a
Felipe Martins da Silva <felipe.dasilva@linx.com.br>
2018-09-28 16:01:40 -0300

    Magic Table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NgxDatatableModule  } from '@swimlane/ngx-datatable'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    NgxDatatableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
